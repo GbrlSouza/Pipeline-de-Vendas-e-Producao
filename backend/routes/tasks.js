03a770e6db77896359d4f2ebec85c7429cc09c34
Gabriel C. de Souza <gabrielsouza_1909@hotmail.com>
2024-12-11 15:29:37 -0300

    Upgrade de informações e funcionalidades

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -1,10 +1,10 @@
+const tasksController = require('../controllers/tasksController');
 const express = require('express');
 const router = express.Router();
-const tasksController = require('../controllers/tasksController');
 
 router.post('/', tasksController.addTask);
 router.get('/', tasksController.getTasks);
-router.put('/:id', tasksController.updateTask);
-router.delete('/:id', tasksController.deleteTask);
+router.put('/', tasksController.updateTask);
+router.delete('/', tasksController.deleteTask);
 
 module.exports = router;
